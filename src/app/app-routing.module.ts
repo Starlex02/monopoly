12503f6b8ae426b8b4473b51fcc5fc86debecfff
DChumak24 <d.chumak@crm.studio>
2023-10-12 22:09:17 +0300

    configure route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { GameSessionComponent } from './game-session/game-session.component';
 
 const routes: Routes = [
   {
-    path: 'game-session',
+    path: '',
     component: GameSessionComponent,
   }
 ];
