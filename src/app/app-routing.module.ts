2e8b3c948d57d04150c05b31507b92cab98a8947
DChumak24 <d.chumak@crm.studio>
2023-10-12 18:00:53 +0300

    config routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { GameSessionComponent } from './game-session/game-session.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'game-session',
+    component: GameSessionComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
