07ff4d9328b5dd0676471750f48b17211aee6977
DChumak24 <d.chumak@crm.studio>
2024-04-09 12:07:53 +0300

    angula init

--- monopoly-app/README.md
+++ monopoly-app/README.md
@@ -1,4 +1,4 @@
-# Monopoly
+# MonopolyApp
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.6.
 
--- monopoly-app/src/app/app.module.ts
+++ monopoly-app/src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GameSessionComponent } from './game-session/game-session.component';
-import { MonopolyCardComponent } from './monopoly-card/monopoly-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    GameSessionComponent,
-    MonopolyCardComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
