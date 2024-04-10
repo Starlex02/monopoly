041a3c74a8d0ede832a373da66a7bb035d8af405
DChumak24 <d.chumak@crm.studio>
2024-04-10 15:11:05 +0300

    displaying a square box for all devices

--- src/app/components/game/board/board.component.ts
+++ src/app/components/game/board/board.component.ts
@@ -6,5 +6,52 @@ import { Component } from '@angular/core';
   styleUrls: ['./board.component.css']
 })
 export class BoardComponent {
+  cells: any[] = []; // Дані про клітинки поля
 
+  ngOnInit() {
+    // Додайте ваші дані про клітинки поля
+    this.cells = [
+      { name: '', type: 'start'},
+      { name: '', type: 'monopoly', pos: 'top' },
+      { name: '', type: 'chance', pos: 'top' },
+      { name: '', type: 'monopoly', pos: 'top' },
+      { name: '', type: 'chance', pos: 'top' },
+      { name: '', type: 'monopoly', pos: 'top' },
+      { name: '', type: 'monopoly', pos: 'top' },
+      { name: '', type: 'chance', pos: 'top' },
+      { name: '', type: 'monopoly', pos: 'top' },
+      { name: '', type: 'monopoly', pos: 'top' },
+      { name: '', type: 'prison' },
+      { name: '', type: 'monopoly', pos: 'side' },
+      { name: '', type: 'empty'},
+      { name: '', type: 'monopoly', pos: 'side' },
+      { name: '', type: 'chance', pos: 'side' },
+      { name: '', type: 'monopoly', pos: 'side' },
+      { name: '', type: 'monopoly', pos: 'side' },
+      { name: '', type: 'monopoly', pos: 'side' },
+      { name: '', type: 'chance', pos: 'side' },
+      { name: '', type: 'monopoly', pos: 'side' },
+      { name: '', type: 'monopoly', pos: 'side' },
+      { name: '', type: 'monopoly', pos: 'side' },
+      { name: '', type: 'monopoly', pos: 'side' },
+      { name: '', type: 'monopoly', pos: 'side' },
+      { name: '', type: 'chance', pos: 'side' },
+      { name: '', type: 'chance', pos: 'side' },
+      { name: '', type: 'monopoly', pos: 'side' },
+      { name: '', type: 'monopoly', pos: 'side' },
+      { name: '', type: 'monopoly', pos: 'side' },
+      { name: '', type: 'monopoly', pos: 'side' },
+      { name: '', type: 'prison' },
+      { name: '', type: 'monopoly', pos: 'top' },
+      { name: '', type: 'monopoly', pos: 'top' },
+      { name: '', type: 'monopoly', pos: 'top' },
+      { name: '', type: 'monopoly', pos: 'top' },
+      { name: '', type: 'monopoly', pos: 'top' },
+      { name: '', type: 'monopoly', pos: 'top' },
+      { name: '', type: 'monopoly', pos: 'top' },
+      { name: '', type: 'chance', pos: 'top' },
+      { name: '', type: 'monopoly', pos: 'top' },
+      { name: '', type: 'jackpot' },
+    ];
+  }
 }
