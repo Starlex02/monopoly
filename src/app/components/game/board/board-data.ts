733855e9849a03bf136b973adc4009a935879492
DChumak24 <d.chumak@crm.studio>
2024-04-11 10:18:27 +0300

    format boardCells

--- src/app/components/game/board/board-data.ts
+++ src/app/components/game/board/board-data.ts
@@ -1,43 +1,258 @@
 export const boardCells = [
-  { name: '', type: 'start' },
-  { name: '', type: 'monopoly', pos: 'top', group: 1, cost: 100 },
-  { name: '', type: 'chance', pos: 'top' },
-  { name: '', type: 'monopoly', pos: 'top', group: 1, cost: 150 },
-  { name: '', type: 'chance', pos: 'top' },
-  { name: '', type: 'monopoly', pos: 'top', group: 2, cost: 1000 },
-  { name: '', type: 'monopoly', pos: 'top', group: 3, cost: 300 },
-  { name: '', type: 'chance', pos: 'top' },
-  { name: '', type: 'monopoly', pos: 'top', group: 3, cost: 300 },
-  { name: '', type: 'monopoly', pos: 'top', group: 3, cost: 350 },
-  { name: '', type: 'prison' },
-  { name: '', type: 'monopoly', pos: 'leftSide', group: 10, cost: 1550 },
-  { name: '', type: 'empty' },
-  { name: '', type: 'monopoly', pos: 'rightSide', group: 4, cost: 500 },
-  { name: '', type: 'chance', pos: 'leftSide' },
-  { name: '', type: 'monopoly', pos: 'rightSide', group: 5, cost: 550 },
-  { name: '', type: 'monopoly', pos: 'leftSide', group: 10, cost: 1500 },
-  { name: '', type: 'monopoly', pos: 'rightSide', group: 4, cost: 500 },
-  { name: '', type: 'chance', pos: 'leftSide' },
-  { name: '', type: 'monopoly', pos: 'rightSide', group: 4, cost: 550 },
-  { name: '', type: 'monopoly', pos: 'leftSide', group: 2, cost: 1000 },
-  { name: '', type: 'monopoly', pos: 'rightSide', group: 2, cost: 1000 },
-  { name: '', type: 'monopoly', pos: 'leftSide', group: 9, cost: 1350 },
-  { name: '', type: 'monopoly', pos: 'rightSide', group: 6, cost: 700 },
-  { name: '', type: 'chance', pos: 'leftSide' },
-  { name: '', type: 'chance', pos: 'rightSide' },
-  { name: '', type: 'monopoly', pos: 'leftSide', group: 9, cost: 1300 },
-  { name: '', type: 'monopoly', pos: 'rightSide', group: 6, cost: 700 },
-  { name: '', type: 'monopoly', pos: 'leftSide', group: 9, cost: 1300 },
-  { name: '', type: 'monopoly', pos: 'rightSide', group: 6, cost: 750 },
-  { name: '', type: 'prison' },
-  { name: '', type: 'monopoly', pos: 'bottom', group: 8, cost: 1150 },
-  { name: '', type: 'monopoly', pos: 'bottom', group: 5, cost: 550 },
-  { name: '', type: 'monopoly', pos: 'bottom', group: 8, cost: 1100 },
-  { name: '', type: 'monopoly', pos: 'bottom', group: 8, cost: 1100 },
-  { name: '', type: 'monopoly', pos: 'bottom', group: 2, cost: 1000 },
-  { name: '', type: 'monopoly', pos: 'bottom', group: 7, cost: 950 },
-  { name: '', type: 'monopoly', pos: 'bottom', group: 7, cost: 900 },
-  { name: '', type: 'chance', pos: 'bottom' },
-  { name: '', type: 'monopoly', pos: 'bottom', group: 7, cost: 900 },
-  { name: '', type: 'jackpot' },
+  {
+    name: '',
+    type: 'start'
+  },
+  {
+    name: '',
+    type: 'monopoly',
+    pos: 'top',
+    group: 1,
+    cost: 100
+  },
+  {
+    name: '',
+    type: 'chance',
+    pos: 'top'
+  },
+  {
+    name: '',
+    type: 'monopoly',
+    pos: 'top',
+    group: 1,
+    cost: 150
+  },
+  {
+    name: '',
+    type: 'chance',
+    pos: 'top'
+  },
+  {
+    name: '',
+    type: 'monopoly',
+    pos: 'top',
+    group: 2,
+    cost: 1000
+  },
+  {
+    name: '',
+    type: 'monopoly',
+    pos: 'top',
+    group: 3,
+    cost: 300
+  },
+  {
+    name: '',
+    type: 'chance',
+    pos: 'top'
+  },
+  {
+    name: '',
+    type: 'monopoly',
+    pos: 'top',
+    group: 3,
+    cost: 300
+  },
+  {
+    name: '',
+    type: 'monopoly',
+    pos: 'top',
+    group: 3,
+    cost: 350
+  },
+  {
+    name: '',
+    type: 'prison'
+  },
+  {
+    name: '',
+    type: 'monopoly',
+    pos: 'leftSide',
+    group: 10,
+    cost: 1550
+  },
+  {
+    name: '',
+    type: 'empty'
+  },
+  {
+    name: '',
+    type: 'monopoly',
+    pos: 'rightSide',
+    group: 4,
+    cost: 500
+  },
+  {
+    name: '',
+    type: 'chance',
+    pos: 'leftSide'
+  },
+  {
+    name: '',
+    type: 'monopoly',
+    pos: 'rightSide',
+    group: 5,
+    cost: 550
+  },
+  {
+    name: '',
+    type: 'monopoly',
+    pos: 'leftSide',
+    group: 10,
+    cost: 1500
+  },
+  {
+    name: '',
+    type: 'monopoly',
+    pos: 'rightSide',
+    group: 4,
+    cost: 500
+  },
+  {
+    name: '',
+    type: 'chance',
+    pos: 'leftSide'
+  },
+  {
+    name: '',
+    type: 'monopoly',
+    pos: 'rightSide',
+    group: 4,
+    cost: 550
+  },
+  {
+    name: '',
+    type: 'monopoly',
+    pos: 'leftSide',
+    group: 2,
+    cost: 1000
+  },
+  {
+    name: '',
+    type: 'monopoly',
+    pos: 'rightSide',
+    group: 2,
+    cost: 1000
+  },
+  {
+    name: '',
+    type: 'monopoly',
+    pos: 'leftSide',
+    group: 9,
+    cost: 1350
+  },
+  {
+    name: '',
+    type: 'monopoly',
+    pos: 'rightSide',
+    group: 6,
+    cost: 700
+  },
+  {
+    name: '',
+    type: 'chance',
+    pos: 'leftSide'
+  },
+  {
+    name: '',
+    type: 'chance',
+    pos: 'rightSide'
+  },
+  {
+    name: '',
+    type: 'monopoly',
+    pos: 'leftSide',
+    group: 9,
+    cost: 1300
+  },
+  {
+    name: '',
+    type: 'monopoly',
+    pos: 'rightSide',
+    group: 6,
+    cost: 700
+  },
+  {
+    name: '',
+    type: 'monopoly',
+    pos: 'leftSide',
+    group: 9,
+    cost: 1300
+  },
+  {
+    name: '',
+    type: 'monopoly',
+    pos: 'rightSide',
+    group: 6,
+    cost: 750
+  },
+  {
+    name: '',
+    type: 'prison'
+  },
+  {
+    name: '',
+    type: 'monopoly',
+    pos: 'bottom',
+    group: 8,
+    cost: 1150
+  },
+  {
+    name: '',
+    type: 'monopoly',
+    pos: 'bottom',
+    group: 5,
+    cost: 550
+  },
+  {
+    name: '',
+    type: 'monopoly',
+    pos: 'bottom',
+    group: 8,
+    cost: 1100
+  },
+  {
+    name: '',
+    type: 'monopoly',
+    pos: 'bottom',
+    group: 8,
+    cost: 1100
+  },
+  {
+    name: '',
+    type: 'monopoly',
+    pos: 'bottom',
+    group: 2,
+    cost: 1000
+  },
+  {
+    name: '',
+    type: 'monopoly',
+    pos: 'bottom',
+    group: 7,
+    cost: 950
+  },
+  {
+    name: '',
+    type: 'monopoly',
+    pos: 'bottom',
+    group: 7,
+    cost: 900
+  },
+  {
+    name: '',
+    type: 'chance',
+    pos: 'bottom'
+  },
+  {
+    name: '',
+    type: 'monopoly',
+    pos: 'bottom',
+    group: 7,
+    cost: 900
+  },
+  {
+    name: '',
+    type: 'jackpot'
+  },
 ];
