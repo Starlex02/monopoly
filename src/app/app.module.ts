09734e931feeda89870afa2d984cb1fb4f5475f2
DChumak24 <d.chumak@crm.studio>
2024-04-11 14:15:48 +0300

    init game-session component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { BoardComponent } from './components/game/board/board.component';
 import { CellComponent } from './components/game/cell/cell.component';
 import { PopupCardComponent } from './components/game/popup-card/popup-card.component';
+import { GameSessionComponent } from './views/game-session/game-session.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BoardComponent,
     CellComponent,
-    PopupCardComponent
+    PopupCardComponent,
+    GameSessionComponent
   ],
   imports: [
     BrowserModule,
