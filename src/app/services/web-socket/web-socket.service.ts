7f2945a28eb3a99a34813b7ad62580d64939c1cc
DChumak24 <d.chumak@crm.studio>
2024-04-18 13:55:44 +0300

    fixing the use of a web socket before it is connected

--- src/app/services/web-socket/web-socket.service.ts
+++ src/app/services/web-socket/web-socket.service.ts
@@ -5,14 +5,30 @@ import { Injectable } from '@angular/core';
 })
 export class WebSocketService {
   private socket: WebSocket;
+  private connectedCallback!: () => void;
 
   constructor() {
     this.socket = new WebSocket('ws://localhost:4201');
+    
+    this.socket.onopen = () => {
+      console.log('Connected to server');
+      if (this.connectedCallback) {
+        this.connectedCallback(); // Викликаємо колбек після успішного підключення
+      }
+    };
+
+    this.socket.onmessage = (event) => {
+      console.log(`Received message: ${event.data}`);
+    };
+
+    this.socket.onclose = () => {
+      console.log('Connection closed');
+    };
   }
 
   connect() {
     this.socket.onopen = () => {
-      console.log('Connected to server');
+      this.socket.send('message');
     };
 
     this.socket.onmessage = (event) => {
@@ -30,5 +46,9 @@ export class WebSocketService {
 
   sendMessage(message: string) {
     this.socket.send(message);
+  }
+
+  onConnected(callback: () => void) {
+    this.connectedCallback = callback;
   }
 }
