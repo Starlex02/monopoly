7c9f133fd89be74b133ebbe13a39221413c87611
DChumak24 <d.chumak@crm.studio>
2024-05-16 10:10:10 +0300

    Реалізовано генерацію кольору світлих тонів

--- server/server.ts
+++ server/server.ts
@@ -595,12 +595,7 @@ function deletePlayerPropertyOwnership(socketId: string){
 }
 
 function getRandomColor() {
-  const letters = '0123456789ABCDEF';
-  let color = '#';
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-
+  const color = "hsl(" + Math.floor(Math.random() * 360) + ",100%,75%)";
   return color;
 }
 
