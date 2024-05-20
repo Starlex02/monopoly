b47e71e6075df479d2e0fc87ff8a2224ea229451
DChumak24 <d.chumak@crm.studio>
2024-05-20 12:39:11 +0300

    Стилізовано карточку гравця

--- src/app/components/game/player-card/player-card.component.ts
+++ src/app/components/game/player-card/player-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, KeyValueDiffer, KeyValueDiffers } from '@angular/core';
 
 @Component({
   selector: 'player-card',
@@ -7,4 +7,40 @@ import { Component, Input } from '@angular/core';
 })
 export class PlayerCardComponent {
   @Input() player: any;
+
+  progress: string = '0';
+  private differ: KeyValueDiffer<any, any>;
+  strokeColor: any;
+
+  constructor(private differs: KeyValueDiffers) {
+    this.differ = this.differs.find({}).create();
+  }
+
+  ngDoCheck(): void {
+    const changes = this.differ.diff(this.player);
+    if (changes) {
+      changes.forEachChangedItem(item => {
+        if (item.key === 'timer') {
+          this.updateProgressAndColor();
+        }
+      });
+    }
+  }
+
+  private updateProgressAndColor(): void {
+    const timerValue = this.player?.timer || 0;
+    const progressPercentage = this.calculateProgress(timerValue);
+    this.progress = progressPercentage.toString();
+
+    const maxColor = 180;
+    const minColor = 0;
+
+    const color = maxColor - (progressPercentage / 100 * (maxColor - minColor));
+
+    this.strokeColor = `hsl(${color}, 100%, 50%)`;
+  }
+
+  private calculateProgress(timerValue: number): number {
+    return (timerValue / 120) * 100;
+  }
 }
